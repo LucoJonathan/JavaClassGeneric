c27cb56e9796d8d11efe8b6502889a9297b0321d
Jonathan Luco <jonathan.luco44@gmail.com>
2025-01-28 11:27:59 +0100

    feat: ServiceGeneric

--- generic/src/main/java/fr/jonathan/generic/ServiceGeneric/ServiceGeneric.java
+++ generic/src/main/java/fr/jonathan/generic/ServiceGeneric/ServiceGeneric.java
@@ -2,13 +2,40 @@ package fr.jonathan.generic.ServiceGeneric;
 
 import fr.jonathan.generic.dto.DtoGeneric;
 import fr.jonathan.generic.entity.EntityGeneric;
+import fr.jonathan.generic.mapper.MapperGeneric;
 import fr.jonathan.generic.repository.RepositoryGeneric;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public abstract class ServiceGeneric<E extends EntityGeneric, D extends DtoGeneric> {
 
     @Autowired
     private RepositoryGeneric<E> repositoryGeneric;
+
+    @Autowired
+    private MapperGeneric<E, D> mapperGeneric;
+
+    public D save(D dto){
+        E entity = mapperGeneric.toEntity(dto);
+        entity = repositoryGeneric.save(entity);
+        return mapperGeneric.toDto(entity);
+    }
+
+    public Optional<D> findById(Long id){
+        return repositoryGeneric.findById(id).map(mapperGeneric::toDto);
+    }
+
+    public D update(D dto){
+        E entity = mapperGeneric.toEntity(dto);
+        repositoryGeneric.saveAndFlush(entity);
+        return mapperGeneric.toDto(entity);
+    }
+
+    public void delete(Long id){
+        repositoryGeneric.deleteById(id);
+    }
+
 }
--- generic/src/main/java/fr/jonathan/generic/mapper/MapperGeneric.java
+++ generic/src/main/java/fr/jonathan/generic/mapper/MapperGeneric.java
@@ -3,7 +3,7 @@ package fr.jonathan.generic.mapper;
 import org.mapstruct.Mapper;
 
 @Mapper
-public interface mapperGeneric<D, E> {
+public interface MapperGeneric<E, D> {
     D toDto(E entity);
     E toEntity(D dto);
 }
