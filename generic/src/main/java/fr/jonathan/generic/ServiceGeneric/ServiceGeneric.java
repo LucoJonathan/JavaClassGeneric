be5e80d84b625b6eac2668f444a0af4c2ecbb0e2
Jonathan Luco <jonathan.luco44@gmail.com>
2025-01-28 10:51:11 +0100

    feat: repo add, mapper missing

--- generic/src/main/java/fr/jonathan/generic/ServiceGeneric/ServiceGeneric.java
+++ generic/src/main/java/fr/jonathan/generic/ServiceGeneric/ServiceGeneric.java
@@ -2,9 +2,13 @@ package fr.jonathan.generic.ServiceGeneric;
 
 import fr.jonathan.generic.dto.DtoGeneric;
 import fr.jonathan.generic.entity.EntityGeneric;
+import fr.jonathan.generic.repository.RepositoryGeneric;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public abstract class ServiceGeneric<E extends EntityGeneric, D extends DtoGeneric> {
 
+    @Autowired
+    private RepositoryGeneric<E> repositoryGeneric;
 }
